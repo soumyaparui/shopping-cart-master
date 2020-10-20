6546f97d1f139d1fc59489bd5cda3658065456f3
133327 <133327@host.docker.internal>
2020-10-20 16:36:04 +0800

    Fix for remediation :FileSystem-Write-FileWriter - Write and Append

--- web-module/src/main/java/org/ejb/sample/servlet/helper/FileUtility.java
+++ web-module/src/main/java/org/ejb/sample/servlet/helper/FileUtility.java
@@ -2,17 +2,19 @@ package org.ejb.sample.servlet.helper;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import com.amazoneaws.auth.AWSStaticCredentialsProvider;
 
 public class FileUtility {
 
-        void generateReportFile(String content) throws IOException {
+    void generateReportFile(String content) throws IOException {
+        FileWriter fileWriter = new FileWriter("~/DailyReports/Report.csv");
+        fileWriter.write(content);
+        fileWriter.close();
+    }
 
-            FileWriter fileWriter = new FileWriter("~/DailyReports/Report.csv");
-            fileWriter.write(content);
-            fileWriter.close();
-        }
-
+    public URL uploadS3ObjectAndGetUrl(String bucketName, String uploadContentPath, String ketName) {
+        AmazoneS3 s3client = getS3Client();
+        s3client.putObject(new PutObjectRequest(bucketName, keyName, new File(uploadContentPath)));
+        return s3client.getUrl(bucketName, keyName);
+    }
 }
-
-
-
